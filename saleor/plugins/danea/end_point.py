7d41eb3d7b83948e34feb82c0f3fdc8a3709d798
reeferman <juglair.b@gmail.com>
2020-10-16 17:54:33 +0200

    benato

--- saleor/plugins/danea/end_point.py
+++ saleor/plugins/danea/end_point.py
@@ -4,26 +4,37 @@ from django.http import HttpResponse
 import logging
 from saleor.plugins.danea.xml_converter import process_product_xml, \
     create_orders
+from django.contrib.auth import authenticate
 
 logger = logging.getLogger(__name__)
 
 
 def process(request):
-    if request.method == 'POST':
-        file = request.FILES.get('file')
-        logger.info("Processing danea request...")
-        discarted: [str] = process_product_xml(file)
-        if len(discarted) > 0:
-            return HttpResponse("Discarted products :" + discarted.__str__(), status=200)
-        else:
-            return HttpResponse("OK")
-    if request.method == 'GET':
-        file = create_orders()
-        logger.info(ElementTree.tostring(file))
-        return HttpResponse(ElementTree.tostring(file), content_type='application/xml')
+    if auth(request):
+        if request.method == 'POST':
+            file = request.FILES.get('file')
+            logger.info("Processing danea request...")
+            discarted: [str] = process_product_xml(file)
+            if len(discarted) > 0:
+                return HttpResponse("Discarted products :" + discarted.__str__(),
+                                    status=200)
+            else:
+                return HttpResponse("OK")
+        if request.method == 'GET':
+            file = create_orders()
+            logger.info(ElementTree.tostring(file))
+            return HttpResponse(ElementTree.tostring(file),
+                                content_type='application/xml')
+    else:
+        return HttpResponse("Wrong user credentials", status=404)
 
-def authenticate(request):
-    logger.info('Authenticating request')
+
+def auth(request):
+    logger.info('Authenticating request on danea endpoint...')
     username = request.POST['username']
     password = request.POST['password']
-    # return authenticate(request, username=username, password=password)
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        logger.info("User authenticated on danea endpoint ", user)
+        return True
+    return False
