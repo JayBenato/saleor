058255fc63fb96022a83432e1aaf63f41c97480e
reeferman <juglair.b@gmail.com>
2021-03-30 09:33:10 +0200

    TS associate color material and size on product update.

--- saleor/plugins/danea/product_manager.py
+++ saleor/plugins/danea/product_manager.py
@@ -33,7 +33,6 @@ def update_product(product: DaneaProduct, warehouse: str):
                     name=variant.size,
                     price_amount=product.gross_price
                 )
-                find_and_associate_size(var, variant)
             try:
                 stock = Stock.objects.get(
                     product_variant_id=var.id,
@@ -51,6 +50,9 @@ def update_product(product: DaneaProduct, warehouse: str):
             stock.save()
             store_variant_private_meta(var, variant)
         find_and_associate_size(var, variant)
+    find_and_associate_color(django_product, product.color)
+    find_and_associate_material(django_product, product.material)
+
 
 def store_variant_private_meta(var: ProductVariant, variant: DaneaVariant):
     private_meta = {
